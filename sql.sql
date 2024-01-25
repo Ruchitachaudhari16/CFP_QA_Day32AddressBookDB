2874fb178c91ff72a774841d82d5b0b0dcd5cec2
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-01-25 17:09:06 +0530

    Create table AddressBook

--- sql.sql
+++ sql.sql
@@ -1,2 +1,13 @@
 Create database AddressBookServiceDB;
 use AddressBookServiceDB;
+CREATE TABLE AddressBook (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    city VARCHAR(255),
+    state VARCHAR(255),
+    zip VARCHAR(10),
+    phone_number VARCHAR(15),
+    email VARCHAR(255)
+);
