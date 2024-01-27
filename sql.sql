73287e93e1baa40387d1453d1e489febea49e55e
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-01-27 14:08:36 +0530

    Insert data into addressbook table

--- sql.sql
+++ sql.sql
@@ -11,3 +11,14 @@ CREATE TABLE AddressBook (
     phone_number VARCHAR(15),
     email VARCHAR(255)
 );
+
+
+		INSERT INTO AddressBook(first_name,last_name,address,city,state,zip,phone_number,email)
+        VALUES 
+		('Ruchita','Chaudhari','RadhaNagar','Amravti','Maharastra',444105,9158834080,'cruchi@gmail.com'),
+		('Rucha','Ingale','Alkachowk','Pune','Maharastra',444105,9158834080,'rucha@gmail.com'),
+		('Nikita', 'Chaudhari','Bypass','Kharadi','Maharastra',444105,9150034080,'cnikii@gmail.com'),
+		('Swati','Lodha','washim','karanja','Maharastra',442105,9158809999,'swati987@gmail.com'),
+		('shama','ingale','rahatni','Amravti','Maharastra',400105,9356763410,'shamaingale@gmail.com'),
+		('Rasikaa','Deshmukh','Pune','Kharadi','Maharastra',411105,9888888880,'rasika@gmail.com'),
+		('tanavi','banait','Mumbai','Airoli','Maharastra',444100,9345678900,'tanavibanait@gmail.com');
